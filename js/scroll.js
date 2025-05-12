bdab641e18e1c06f04206dafc8d7d121c5b7b88e
Denis Sergeev <denis-sergeev-85@inbox.ru>
2025-05-12 10:32:23 +0300

    подписка на рассылку

---
+++ js/scroll.js
@@ -0,0 +1,10 @@
+document
+  .getElementById("scrollToTopBtn")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
