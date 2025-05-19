9c977d4af39f019c21058afb3e34e2a5da0d4aec
Denis Sergeev <denis-sergeev-85@inbox.ru>
2025-05-19 12:19:20 +0300

    скролл наверх

--- js/scroll.js
+++ js/scroll.js
@@ -8,3 +8,11 @@ document
       behavior: "smooth",
     });
   });
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 100) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+});
